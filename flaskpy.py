8c89a181b83c5f60c862354ecaf651c399e4811e
DandaIT04 <s10208266@connect.np.edu.sg>
2023-01-03 16:07:05 +0800

    Updated index and flaskpy file

--- flaskpy.py
+++ flaskpy.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask,render_template,redirect,url_for
+
+# for vscode, type python flaskpy.py in terminal to run at 5221 port
 
 app = Flask(__name__)
 
-@app.route("index.html")
+@app.route("/")
+def redirectToMain():
+    return redirect(url_for("hello_world"))
+
+@app.route("/main")
 def hello_world():
-    return "<p>Hello, World!</p>
+    helloVar = "Hello world"
+    return render_template("index.html",helloVar=helloVar)
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5221,host="localhost")
